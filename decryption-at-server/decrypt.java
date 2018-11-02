c6b11ec64b0d1411a6aa1f49e9419a181312b9b8
Hardik Jain <hardik@Hardiks-MacBook-Pro.local>
2018-11-01 20:46:03 -0700

    UPDATE

---
+++ decryption-at-server/decrypt.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import main.Embed;
+import main.Extract;
+
+
+class decrypt
+{
+	public static void main(String args[])
+	{
+		String arg[]={"qwertyasdfgh","details.txt","Stego.jpg"};
+		Extract.main(arg);
+		aes.input();
+		//System.out.println(ans);
+		
+
+	}
+}
